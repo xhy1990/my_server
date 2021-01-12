9d841029e6642e78fcfc288dafae0dd66205e5b7
hangyu.xue@onemena.com <hangyu.xue@onemena.com>
2021-01-12 14:51:41 +0800

    新建tool模块

---
+++ xx_tools/src/main/java/xx/love/cc/HelloWorld.java
@@ -0,0 +1,13 @@
+package xx.love.cc;
+
+/**
+ * 😊
+ *
+ * @author xhy
+ * @date 2021/1/12 14:48
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("👌😍😍😍😍😍😍😍😍");
+    }
+}
